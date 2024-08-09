ebb54b91ff1c7a5ced4467d3728ebec146432bd4
Armatys Konrad <karmatys888@gmail.com>
2024-08-09 10:04:44 +0200

    Circle gestures

--- src/lessons/CircleGestures/CircleGestures.tsx
+++ src/lessons/CircleGestures/CircleGestures.tsx
@@ -1,14 +1,68 @@
 import { Container } from "@/components/Container";
 import { hitSlop } from "@/lib/reanimated";
 import { colorShades, layout } from "@/lib/theme";
+import { useMemo } from "react";
 import { StyleSheet, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 export function CircleGesturesLesson() {
+  const scale = useSharedValue(1);
+  const xOffset = useSharedValue(0);
+
+  const pan = useMemo(
+    () =>
+      Gesture.Pan()
+        .onBegin(() => {
+          scale.value = 2;
+        })
+        .onChange((ev) => {
+          xOffset.value += ev.changeX;
+        })
+        .onEnd(() => {
+          xOffset.value = withSpring(0);
+        })
+        .onFinalize(() => {
+          scale.value = withSpring(1);
+        }),
+    []
+  );
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      borderWidth: interpolate(
+        scale.value,
+        [1, 2],
+        [layout.knobSize / 2, 2],
+        Extrapolation.CLAMP
+      ),
+      transform: [
+        {
+          translateX: xOffset.value,
+        },
+        {
+          scale: scale.value,
+        },
+      ],
+    };
+  });
+
   return (
     <Container>
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <View style={[styles.knob]} hitSlop={hitSlop} />
-      </View>
+      <GestureDetector gesture={pan}>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Animated.View
+            style={[styles.knob, animatedStyle]}
+            hitSlop={hitSlop}
+          />
+        </View>
+      </GestureDetector>
     </Container>
   );
 }
